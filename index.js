5a4bbde18fc659896a4089658d749e1bf2b321f0
Valeco96 <correalev2@gmail.com>
2025-06-05 18:04:39 +0200

    finito lo svolgimento degli esercizi week-1 day-2

--- index.js
+++ index.js
@@ -11,20 +11,17 @@ if (x === 50 || y === 50 || x + y === 50) {
 
 //////Esercizio 2///////
 
-let frase = "Here comes the sun";
-console.log(frase);
-let parola = "sun";
+let frase = "Hello, everyone!";
+posizione = 5;
 
-//Trova la posizione della parola "sun"
-let posizione = frase.search(parola);
-console.log(posizione);
-
-function sostituisciParola(frase, posizione) {
-  let nuovaParola = "rain";
-  let nuovaFrase = frase.replace(parola, nuovaParola);
-  console.log(nuovaFrase);
-  return nuovaFrase;
+function rimuoviCarattere(frase, posizione) {
+  //Taglia la parte prima e dopo il carattere da rimuovere, poi le unisce
+  return frase.slice(0, posizione) + frase.slice(posizione + 1);
 }
+
+let nuovaFrase = rimuoviCarattere(frase, posizione);
+
+console.log(nuovaFrase);
 
 ///////Esercizio 3//////////
 
@@ -39,20 +36,96 @@ if ((a > 40 && a < 60) || (b > 70 && b < 90)) {
 
 /////////Esercizio 4////////
 
-let city = prompt("Inserisci il nome di una citta'");
+//let city = prompt("Inserisci il nome di una citta'");
 
 function mostraCitta(cityName) {
-  if (cityName.startsWith("Los") || cityName.startsWith("New")) {
-    console.log(true);
+  let lowerCaseCities = cityName.toLowerCase();
+
+  if (lowerCaseCities.startsWith("los") || lowerCaseCities.startsWith("new")) {
+    console.log(cityName);
   } else {
     console.log(false);
   }
 }
 
+//let citta = mostraCitta(city);
+//console.log(citta);
+
 ////////////Esercizio 5////////
 
 let numeri = [45, 89, 2, 8, 12];
 
-function calcolaSomma() {
-    
+function calcolaSomma(numeri) {
+  let somma = numeri.reduce((acc, numero) => acc + numero, 0);
+  //acc = accumulatore (la somma totale fino ad ora)
+  //numero = elemento corrente dell'array mentre .reduce() scorre
+  //acc + numero = la somma parziale, che diventa il nuovo valore dell'accumulatore
+  //0 = valore iniziale da cui parte la somma
+  //Cosa succede internamente: acc = 0, numero = 45 => acc = 0 + 45 = 45; e cosí via.
+  return somma;
 }
+
+let risultato = calcolaSomma(numeri);
+console.log(risultato);
+
+/////////Esercizio 6//////////
+
+let listaNumeri = [12, 25, 40];
+
+function controllaNumero(lista) {
+  for (let i = 0; i < listaNumeri.length; i++) {
+    if (listaNumeri.includes(1) || listaNumeri.includes(3)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+let checkNumeri = controllaNumero(listaNumeri);
+console.log(checkNumeri);
+
+////////Esercizio 7///////////
+
+let angolo = 90;
+
+function geometria(angolo) {
+  if (angolo < 90) {
+    return "acuto";
+  } else if (angolo === 90) {
+    return "retto;";
+  } else if (angolo > 90 && angolo < 180) {
+    return "ottuso";
+  } else {
+    if (angolo === 180) {
+      return "piatto";
+    }
+  }
+}
+
+let definizioneAngoli = geometria(angolo);
+console.log(definizioneAngoli);
+
+////////Esercizio 8/////////
+
+let sentence = "Fabbrica Italiana Automobili Torino";
+
+function creaAcronimo() {
+  //Dividi la frase in parole
+  let parole = sentence.toLowerCase().split(` `);
+
+  //Estrai la prima lettera di ogni parola
+  let lettere = [];
+  parole.forEach((parola) => {
+    lettere.push(parola[0]);
+  });
+
+  //unisci le lettere
+  let acronimo = lettere.join(``);
+
+  //Trasforma in maiuscolo
+  return acronimo.toUpperCase();
+}
+
+let sigla = creaAcronimo(sentence);
+console.log(sigla);
